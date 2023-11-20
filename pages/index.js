6a345feea09084c4949c97e32e1276a45ae08176
Jeff <doylejeff98@gmail.com>
2023-11-20 01:17:08 -0500

    side bar in newly created components folder

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Sidebar from '@/components/Sidebar';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+      <Sidebar />
+      { /*<Postsfeed /> */}
+      { /* <Trending />*/}
     </>
   )
 }
