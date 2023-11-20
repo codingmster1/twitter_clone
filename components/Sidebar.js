00f5645253c2c27e52eb5adb683dd03bbd475090
Jeff <doylejeff98@gmail.com>
2023-11-20 02:36:37 -0500

    mobile responsivenss for nav

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,22 +1,30 @@
+import { HomeIcon } from '@heroicons/react/outline';
+
+
 export default function Sidebar() {
     return (
         <div className="hidden sm:flex flex-col">
             <div>Logo</div>
             <nav>
-                <SidebarLink text={"Home"} />
-                <SidebarLink text={"Explore"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                <SidebarLink Icon={HomeIcon} text={"Home"} />
+                {/* <SidebarLink text={"Explore"} />
                 <SidebarLink text={"Notifications"} />
-                <SidebarLink text={"Messages"} />
+                <SidebarLink text={"Messages"} /> */}
             </nav>
             <div>User</div>
         </div>
     )
 }
 
-function SidebarLink({ text, icon }) {
+function SidebarLink({ text, Icon }) {
     return (
-        <li>
-            {text}
+        <li className="flex mb-6 items-center text-xl">
+            <Icon className="h-7" />
+            <span className="hidden xl:inline">{text}</span>
         </li>
     )
 }
