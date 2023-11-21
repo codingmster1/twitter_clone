028b691f3b0bfd32a538c40d747c9ee83041e243
Jeff <doylejeff98@gmail.com>
2023-11-20 19:25:03 -0500

    main sidebar links done

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,9 +13,9 @@ import Image from 'next/image';
 
 export default function Sidebar() {
     return (
-        <div className="hidden sm:flex flex-col fixed font-Zelda"> {/*add hidden later in the beginning of function */}
-            <nav className="xl:space-y-1.5">
-                <div className="py-3 xl:p-3">
+        <div className="h-full hidden sm:flex flex-col fixed xl:ml-24 font-Zelda"> {/*add hidden later in the beginning of function */}
+            <nav className="h-full relative xl:space-y-1.5">
+                <div className="flex justify-center item-center xl:justify-start py-3 xl:p-3">
                     <Image src={'/assets/cucco2.png'} width={34} height={34} />
                 </div>
                 <SidebarLink Icon={HomeIcon} text={"Home"} />
@@ -25,15 +25,16 @@ export default function Sidebar() {
                 <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"} />
                 <SidebarLink Icon={UserIcon} text={"Profile"} />
                 <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
+                <div className="absolute bottom-0">User</div>
             </nav>
-            <div>User</div>
+
         </div>
     )
 }
 
 function SidebarLink({ text, Icon }) {
     return (
-        <li className="hoverAnimation flex mb-6 xl:justify-start justify-center items-center text-xl space-x-3">
+        <li className="hoverAnimation flex mb-3 xl:justify-start justify-center items-center text-xl space-x-3">
             <Icon className="h-7" />
             <span className="hidden xl:inline">{text}</span>
         </li>
