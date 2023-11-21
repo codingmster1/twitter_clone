6050819e5634c853b439179d37df90e93634a089
Jeff <doylejeff98@gmail.com>
2023-11-20 21:12:37 -0500

    post feed classes and trending file with border

---
+++ components/Postfeed.js
@@ -0,0 +1,11 @@
+export default function PostFeed() {
+    return (
+        <div className="sm:ml-20 xl:ml-96 max-w-2xl flex-grow
+        border-green-700 border-x">
+            <div className="px-3 py-2 text-lg sm:text-xl font-bold">
+                Home
+            </div>
+            Feed
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Sidebar from '@/components/Sidebar';
+import PostFeed from "@/components/Postfeed";
+import Trending from '@/components/Trending';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className="bg-[#d4fdf6] min-h-screen text-[#000000]
-    max-w-[1400px] mx-auto font-Zelda">
+    max-w-[1400px] mx-auto flex font-Zelda">
       <Sidebar />
-      { /*<Postfeed /> 
-        <Trending />*/}
+      <PostFeed />
+      <Trending />
     </div>
   );
 }
