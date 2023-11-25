7870f0bfac1184eb409c8c60e1f20886e3640e02
Jeff <doylejeff98@gmail.com>
2023-11-25 00:55:14 -0500

    copied and pasted 4 more divs for whats happening

--- components/Trending.js
+++ components/Trending.js
@@ -5,7 +5,7 @@ export default function Trending() {
         <div className="lg:flex font-Quest">
             Trending
 
-            <div className="flex space-x-3 bg-white 
+            <div className="flex space-x-3 bg-white
             w-[300px] h-[44px] p-3 rounded-3xl">
                 <SearchIcon className="w-6" />
                 <input
@@ -18,13 +18,40 @@ export default function Trending() {
             </div>
             <div className="w-[300px] h-[500px] bg-white rounded-3xl mt-3">
                 <h1 className="font-Quest text-xl p-3">Whats been up</h1>
-                <div className="relative">
+                <div className="p-3 relative">
                     <DotsCircleHorizontalIcon className="w-5 absolute right-4" />
-                    <p>Trending in US</p>
-                    <h1>China</h1>
-                    <p>340k Tweets</p>
-
+                    <p className="text-xs text-gray-500 font-Quest">Trending in US</p>
+                    <h1 className="text-[15px] font-bold font-Quest">China</h1>
+                    <p className="text-xs text-gray-500 font-Quest">340k Tweets</p>
                 </div>
+                <div className="p-3 relative">
+                    <DotsCircleHorizontalIcon className="w-5 absolute right-4" />
+                    <p className="text-xs text-gray-500 font-Quest">Trending in US</p>
+                    <h1 className="text-[15px] font-bold font-Quest">China</h1>
+                    <p className="text-xs text-gray-500 font-Quest">340k Tweets</p>
+                </div>
+                <div className="p-3 relative">
+                    <DotsCircleHorizontalIcon className="w-5 absolute right-4" />
+                    <p className="text-xs text-gray-500 font-Quest">Trending in US</p>
+                    <h1 className="text-[15px] font-bold font-Quest">China</h1>
+                    <p className="text-xs text-gray-500 font-Quest">340k Tweets</p>
+                </div>
+                <div className="p-3 relative">
+                    <DotsCircleHorizontalIcon className="w-5 absolute right-4" />
+                    <p className="text-xs text-gray-500 font-Quest">Trending in US</p>
+                    <h1 className="text-[15px] font-bold font-Quest">China</h1>
+                    <p className="text-xs text-gray-500 font-Quest">340k Tweets</p>
+                </div>
+                <div className="p-3 relative">
+                    <DotsCircleHorizontalIcon className="w-5 absolute right-4" />
+                    <p className="text-xs text-gray-500 font-Quest">Trending in US</p>
+                    <h1 className="text-[15px] font-bold font-Quest">China</h1>
+                    <p className="text-xs text-gray-500 font-Quest">340k Tweets</p>
+                </div>
+
+
+
+
 
             </div>
         </div>
