5df2e72c31ffdd41abd770ed250239694c5bf54b
Jeff <doylejeff98@gmail.com>
2023-11-25 01:19:50 -0500

    created whotofollow box and added profile pic

--- components/Trending.js
+++ components/Trending.js
@@ -1,4 +1,5 @@
 import { DotsCircleHorizontalIcon, SearchIcon } from "@heroicons/react/outline";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
 
 export default function Trending() {
     return (
@@ -49,6 +50,28 @@ export default function Trending() {
                     <p className="text-xs text-gray-500 font-Quest">340k Tweets</p>
                 </div>
 
+
+
+
+
+            </div>
+
+            <div className="w-[300px] h-[300px] bg-white rounded-3xl mt-3">
+                <h1 className="font-Quest text-xl p-3">Who to follow</h1>
+
+                <div>
+                    <div className="flex space-x-3 p-3">
+                        <img className="w-11 h-11 object-cover rounded-full" src="/assets/beedle.jpg" />
+                        <div>
+                            <h1 className="font-bold">Beedle</h1>
+                            <BadgeCheckIcon className="w-[18px] text-blue-400" />
+                        </div>
+
+                    </div>
+                    <button>
+                        Follow
+                    </button>
+                </div>
 
 
 
