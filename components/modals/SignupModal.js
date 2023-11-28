0a2ce17d6764f28c5ca40fd1ca41fe9ecd0cc00e
Jeff <doylejeff98@gmail.com>
2023-11-28 14:50:07 -0500

    sign up modal open and close fix

--- components/modals/SignupModal.js
+++ components/modals/SignupModal.js
@@ -1,5 +1,7 @@
+import { closeSignupModal, openSignupModal } from '@/redux/modalSlice';
 import Modal from '@mui/material/Modal';
 import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function SignupModal() {
 
@@ -8,19 +10,20 @@ export default function SignupModal() {
     //const handleOpen = () => setIsOpen(true)
 
     const isOpen = useSelector(state => state.modals.signupModalOpen)
-
+    const dispatch = useDispatch()
+    console.log(isOpen)
     return (
         <>
             <button className="bg-white text-black font-Quest
                 w-[160px] rounded-full h-[40px] hover:bg-[#cbd2d7]"
-                onClick={handleOpen}
+                onClick={() => dispatch(openSignupModal())}
             >
                 Sign Up
             </button>
 
             <Modal
                 open={isOpen}
-                onClose={handleClose}
+                onClose={() => dispatch(closeSignupModal())}
                 className="flex justify-center items-center">
 
                 <div className="w-[400px] h-[200px] bg-white">
--- redux/store.js
+++ redux/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import modalSlice from './modalSlice'
 
 export const store = configureStore({
     reducer: {
