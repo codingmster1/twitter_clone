b5de6b73757d61588ccd424fddea912f67beb9d2
Jeff <doylejeff98@gmail.com>
2023-11-29 02:16:34 -0500

    create account input field and styling

--- components/modals/SignupModal.js
+++ components/modals/SignupModal.js
@@ -20,15 +20,29 @@ export default function SignupModal() {
                 onClose={() => dispatch(closeSignupModal())}
                 className="flex justify-center items-center">
 
-                <div className="w-[90%] h-[400px] bg-white font-Quest md:w-[560px]
-                md:h-[600px] border border-blue-500 rounded-lg">
+                <div className="w-[90%] h-[600px] bg-white font-Quest md:w-[560px]
+                md:h-[600px] border border-blue-500 rounded-lg
+                flex justify-center">
 
 
-                    <div className="w-[90%]" >
+                    <div className="w-[90%] mt-8 flex flex-col" >
                         <button className="bg-black text-white w-full font-Quest font-bold
                     text-lg p-2">
                             Sign in as Guest
                         </button>
+                        <h1 className="text-center mt-4 font-bold font-Quest text-lg">or</h1>
+                        <h1 className="text-center mt-4 font-bold font-Quest text-2xl">Create your Account</h1>
+                        <input placeholder="Full Name"
+                            className="h-10 mt-8 rounded-md bg-transparent border border-black p-6"
+                            type={"text"} />
+                        <input
+                            placeholder="Email"
+                            className="h-10 mt-8 rounded-md bg-transparent border border-black p-6"
+                            type={"email"} />
+                        <input
+                            placeholder="Password"
+                            className="h-10 mt-8 rounded-md bg-transparent border border-black p-6"
+                            type={"password"} />
                     </div>
                 </div>
 
