069c84a75cab0510b0e2faeeda1ae11e2949312d
Jeff <doylejeff98@gmail.com>
2023-12-03 20:45:38 -0500

    fixed/added close comment modal text area

--- components/modals/CommentModal.js
+++ components/modals/CommentModal.js
@@ -3,18 +3,20 @@ import Modal from '@mui/material/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function CommentModal() {
-    const isOpen = useSelector(state => state.modals.commentModalOpen)
+    const isOpen = useSelector((state) => state.modals.commentModalOpen)
     const dispatch = useDispatch()
+    console.log(isOpen)
     return (
         <>
 
             <Modal
+                className="flex justify-center items-center"
                 open={isOpen}
-                onClose={() => dispatch(closeCommentModal)}
+                onClose={() => dispatch(closeCommentModal())}
 
             >
 
-                <div className="w-[500px] h=[500px]">
+                <div className="bg-white w-[500px] h-[500px]">
                     This is the comment modal
                 </div>
 
