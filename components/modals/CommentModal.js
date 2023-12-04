4cd8172f49d548891c265159e1f929bb7cfea20c
Jeff <doylejeff98@gmail.com>
2023-12-03 20:18:21 -0500

    imported dispatch into comment modal

--- components/modals/CommentModal.js
+++ components/modals/CommentModal.js
@@ -1,10 +1,23 @@
+import { closeCommentModal } from '@/redux/modalSlice';
 import Modal from '@mui/material/Modal';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function CommentModal() {
-    <>
+    const isOpen = useSelector(state => state.modals.commentModalOpen)
+    const dispatch = useDispatch()
+    return (
+        <>
 
-        <Modal>
+            <Modal>
+                open={isOpen}
+                onClose={() => dispatch(closeCommentModal)}
 
-        </Modal>
-    </>
+
+                <div className="w-[500px] h=[500px]">
+                    This is the comment modal
+                </div>
+
+            </Modal>
+        </>
+    )
 }
