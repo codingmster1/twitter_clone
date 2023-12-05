49bf9f5260d87dd4fe7b6828ec716b6a0031ebd1
Jeff <doylejeff98@gmail.com>
2023-12-05 03:18:48 -0500

    delete button in progress/customized

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,6 +1,6 @@
 import { db } from "@/firebase";
 import { openCommentModal, openLoginModal, setCommentTweet } from "@/redux/modalSlice";
-import { HeartIcon as FilledHeartIcon } from "@heroicons/react/solid";
+import { HeartIcon as FilledHeartIcon, TrashIcon } from "@heroicons/react/solid";
 import { ChartBarIcon, ChatIcon, HeartIcon, UploadIcon } from "@heroicons/react/outline"
 import { arrayRemove, arrayUnion, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
@@ -92,6 +92,12 @@ export default function Tweet({ data, id }) {
                         <HeartIcon className="w-5 cursor-pointer hover:text-red-400" />}
                     {likes.length > 0 && <span>{likes.length}</span>}
                 </div>
+                {user.uid === data?.uid && (<div
+                    className="cursor-pointer hover:text-red-600"
+                    onClick={deleteTweet}>
+
+                    <TrashIcon className="w-5" />
+                </div>)}
                 <ChartBarIcon className="w-5 cursor-not-allowed" />
                 <UploadIcon className="w-5 cursor-not-allowed" />
             </div>
