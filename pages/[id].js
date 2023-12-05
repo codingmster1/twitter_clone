b2350c3bc0bd609907ebf133a02ce88a69ce87e3
Jeff <doylejeff98@gmail.com>
2023-12-04 20:41:07 -0500

    3 new icons added for tweetcomment route

--- pages/[id].js
+++ pages/[id].js
@@ -2,6 +2,7 @@ import PostFeed from "@/components/Postfeed";
 import Sidebar from "@/components/Sidebar";
 import Trending from "@/components/Trending";
 import Tweet from "@/components/Tweet";
+import { ArrowLeftIcon } from "@heroicons/react/outline";
 import Image from 'next/image';
 
 export default function CommentsPage() {
@@ -13,11 +14,14 @@ export default function CommentsPage() {
 
                 <div className="sm:ml-16 xl:ml-80 max-w-2xl flex-grow
         border-blue-500 border-x">
-                    <div className="p-3 flex justify-between px-3 py-2 text-lg sm:text-xl font-bold font-Quest
+                    <div className="flex items-center gap-12 p-3  px-3 py-2 text-lg sm:text-xl font-bold font-Quest
             border-b border-blue-500 sticky top-0 z-50">
+                        <ArrowLeftIcon className="w-7"
+                        />
                         Tweet
-                        <Image src={'/assets/skullkid.gif'} width={36} height={34} />
+                        <Image src={'/assets/mmtweeticon.gif'} width={34} height={34} />
                         <Image src={'/assets/commenticon2.gif'} width={34} height={34} />
+                        <Image src={'/assets/dekulink.gif'} width={34} height={34} />
                     </div>
 
 
@@ -26,9 +30,9 @@ export default function CommentsPage() {
 
                     <Tweet />
                 </div>
-            </div>
-            <Trending />
-        </div>
 
+                <Trending />
+            </div>
+        </div>
     )
 }
