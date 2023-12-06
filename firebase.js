13d53c96fa9b4a8bb1ef61d2a13a3c00c30560ca
Jeff <doylejeff98@gmail.com>
2023-12-06 02:25:03 -0500

    secrutiy measure to hide api keys firebase

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDVPNtUhzzkTzRVxdpK-XV8ZF-HOvW6HMo",
-    authDomain: "twitterclone-53240.firebaseapp.com",
-    projectId: "twitterclone-53240",
-    storageBucket: "twitterclone-53240.appspot.com",
-    messagingSenderId: "764641875884",
-    appId: "1:764641875884:web:ab077c947b636f227e0c09"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
